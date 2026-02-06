3b5677c2c66cc969cf034eb104c76d0e385700a2
Tanaya Aswale <tanayaaswale@gmail.com>
2026-02-06 13:56:39 +0530

    Create iam.tf

---
+++ iam.tf
@@ -0,0 +1,34 @@
+resource "aws_iam_role" "lambda_role" {
+  name = "fraud-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "lambda_policy" {
+  role = aws_iam_role.lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "logs:*",
+          "kinesis:*",
+          "dynamodb:*",
+          "sagemaker:InvokeEndpoint"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
